e30eed0023e1d9533299a48d8b0995e4c73144fa
Hola <michael.alberto.zerpa@estudiantat.upc.edu>
2023-04-25 17:20:50 +0200

    perfil usuario
    cambios en relación al perfil del usurario (Fragment y layout) y añadido dependencia en build.grade

--- app/src/main/java/com/example/appptin/UserFragment.java
+++ app/src/main/java/com/example/appptin/UserFragment.java
@@ -10,12 +10,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class UserFragment extends Fragment {
 
-    ImageView foto_perfil;
+    CircleImageView foto_perfil;
     Button cerrar_sesion;
     public UserFragment() {
         // Required empty public constructor
