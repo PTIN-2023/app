e0c22bb3939ba308176dbcea97e26c70bb2151d3
etz00 <115375641+etz00@users.noreply.github.com>
2023-05-13 18:22:16 +0200

    Pantalla mètode pagament + funcionalitats

--- app/src/main/java/com/example/appptin/PagamentActivity.java
+++ app/src/main/java/com/example/appptin/PagamentActivity.java
@@ -3,12 +3,59 @@ package com.example.appptin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class PagamentActivity extends AppCompatActivity {
+
+    private LinearLayout cardDetailsLayout;
+    private boolean CardLayoutVisible = false;
+    private LinearLayout paypalDetailsLayout;
+    private boolean paypalLayoutVisible = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagament);
+
+        cardDetailsLayout = findViewById(R.id.card_details_layout);
+        paypalDetailsLayout = findViewById(R.id.paypal_details_layout);
+
+        Button btnPaypal = findViewById(R.id.btn_paypal_payment);
+
+        btnPaypal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPayPalDetails(v);
+            }
+        });
+    }
+
+    public void showCardDetails(View view) {
+
+        if (CardLayoutVisible) {
+            cardDetailsLayout.setVisibility(View.GONE);
+            CardLayoutVisible = false;
+        } else {
+            cardDetailsLayout.setVisibility(View.VISIBLE);
+            CardLayoutVisible = true;
+        }
+    }
+
+    public void showPayPalDetails(View view) {
+
+        if (paypalLayoutVisible) {
+            paypalDetailsLayout.setVisibility(View.GONE);
+            paypalLayoutVisible = false;
+        } else {
+            paypalDetailsLayout.setVisibility(View.VISIBLE);
+            paypalLayoutVisible = true;
+        }
+    }
+
+    public void processCardPayment(View view) {
+        // Processament pagament amb targeta
     }
 }
