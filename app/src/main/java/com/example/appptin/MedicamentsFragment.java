a63db30227ba621cce695855ea0c5616a5a0adbd
etz00 <115375641+etz00@users.noreply.github.com>
2023-05-22 00:06:12 +0200

    Pàg. Imatges Medicaments NO URL (+-)

--- app/src/main/java/com/example/appptin/MedicamentsFragment.java
+++ app/src/main/java/com/example/appptin/MedicamentsFragment.java
@@ -1,6 +1,8 @@
 package com.example.appptin;
 
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,10 +11,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -25,6 +32,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -43,6 +53,8 @@ public class MedicamentsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private LinearLayout linearLayoutMedicaments;
 
     public MedicamentsFragment() {
         // Required empty public constructor
@@ -81,11 +93,24 @@ public class MedicamentsFragment extends Fragment {
         // Inflate the layout for this fragment
         super.onCreate(savedInstanceState);
         View view = inflater.inflate(R.layout.fragment_medicaments, container, false);
+        linearLayoutMedicaments = view.findViewById(R.id.medicaments_layout);
+
         RequestQueue queue = Volley.newRequestQueue(getActivity());
         Resources r = getResources();
         String apiUrl = r.getString(R.string.api_base_url);
         String url = apiUrl + "/api/list_available_medicines"; // Reemplaça amb l'adreça completa de l'API per obtenir els medicaments disponibles
         JSONObject jsonBody = new JSONObject();
+
+        // Ejemplo de medicamentos de prueba
+        List<Medicament> medicaments = new ArrayList();
+        medicaments.add(new Medicament("Medicamento 1", "123456789", "Uso 1", "Administración 1", true, 10.0, "Forma 1", "Excipiente 1"));
+        medicaments.add(new Medicament("Medicamento 2", "987654321", "Uso 2", "Administración 2", false, 15.0, "Forma 2", "Excipiente 2"));
+        medicaments.add(new Medicament("Medicamento 3", "567890123", "Uso 3", "Administración 3", true, 20.0, "Forma 3", "Excipiente 3"));
+        medicaments.add(new Medicament("Medicamento 4", "321098765", "Uso 4", "Administración 4", false, 25.0, "Forma 4", "Excipiente 4"));
+
+        // Llamar a la función para agregar las vistas de los medicamentos
+        agregarVistasMedicamentos(medicaments);
+
         try {
             jsonBody.put("session_token", login.getSession_token());
         //    jsonBody.put("filter", False);
@@ -138,6 +163,9 @@ public class MedicamentsFragment extends Fragment {
                             medicaments.add(new Medicament(name, nationalCode, useType, typeOfAdministration,
                                     prescriptionNeeded, pvp, form, excipient));
                         }
+
+
+
                         // Aquí tens la llista de medicaments, pots fer amb ella el que necessitis
                     } else {
                         // En cas que el resultat sigui diferent de "ok"
@@ -166,6 +194,70 @@ public class MedicamentsFragment extends Fragment {
 
 
         return view;
+    }
+
+    // Función para agregar las vistas de los medicamentos
+    private void agregarVistasMedicamentos(List<Medicament> medicaments) {
+        //LinearLayout scroll_medicaments = getView().findViewById(R.id.scroll_medicaments);
+
+        for (Medicament medicament : medicaments) {
+            // Crear el LinearLayout para cada medicamento
+            LinearLayout medicamentoLayout = new LinearLayout(getActivity());
+            medicamentoLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT));
+            medicamentoLayout.setOrientation(LinearLayout.VERTICAL);
+
+            // Crear el ImageView y cargar la imagen desde la URL
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(
+                    225, // Ancho deseado en píxeles
+                    225  // Alto deseado en píxeles
+            ));
+            // Establecer la imagen de muestra desde el recurso drawable
+            imageView.setImageResource(R.drawable.avatar_gestor);
+            //cargarImagenDesdeURL(medicament.getImageUrl(), imageView);
+
+            // Crear el TextView y configurar el nombre del medicamento
+            TextView nombreTextView = new TextView(getActivity());
+            nombreTextView.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            ));
+            nombreTextView.setText("prova");
+
+            // Agregar el ImageView y el TextView al LinearLayout del medicamento
+            medicamentoLayout.addView(imageView);
+            medicamentoLayout.addView(nombreTextView);
+
+            // Agregar el LinearLayout del medicamento al contenedor principal
+            linearLayoutMedicaments.addView(medicamentoLayout);
+        }
+    }
+
+    // Función para cargar una imagen desde una URL y establecerla en un ImageView
+    private void cargarImagenDesdeURL(String imageUrl, ImageView imageView) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(imageUrl);
+                    InputStream inputStream = url.openConnection().getInputStream();
+                    final Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            imageView.setImageBitmap(bitmap);
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
     }
 
     private void replaceFragments(Fragment fragment){
