135c4d5a723384598f4047d68c6d614a6e3373e1
alexrocamora <72758624+alexrocamora@users.noreply.github.com>
2023-06-28 13:44:44 +0200

    mensaje "cerrar sesión" + logout gestor
    Ara en gestor

--- app/src/main/java/com/example/appptin/gestor/GestorActivity.java
+++ app/src/main/java/com/example/appptin/gestor/GestorActivity.java
@@ -1,14 +1,17 @@
 package com.example.appptin.gestor;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
@@ -28,6 +31,8 @@ import com.example.appptin.gestor.fragments.inventario.MapaGestor;
 import com.example.appptin.gestor.fragments.pefilgestor.PerfilGestorFragment;
 import com.example.appptin.gestor.fragments.pefilgestor.opciones.ConfigGestorFragment;
 import com.example.appptin.gestor.fragments.pefilgestor.opciones.EstatPeticionsGestorFragment;
+import com.example.appptin.medico.MedicoActivity;
+import com.example.appptin.welcome_page;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.json.JSONException;
@@ -183,6 +188,78 @@ public class GestorActivity extends AppCompatActivity  implements ConfigGestorFr
                     }
                 });
         queue.add(jsonObjectRequest);
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Cerrar Sesión")
+                .setMessage("Seguro que quieres salir de tu sesión?")
+                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        logout();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
+    }
+
+    private boolean logout() {
+        boolean exists = false;
+        RequestQueue queue = Volley.newRequestQueue(this);
+        Resources r = getResources();
+        String apiUrl = r.getString(R.string.api_base_url);
+        String url = apiUrl + "/api/logout"; // Reemplaza con la dirección de tu API
+        System.out.println(url);
+        JSONObject jsonBody = new JSONObject();
+        SharedPreferences sharedPreferences = getSharedPreferences("UserPref", Context.MODE_PRIVATE);
+        try {
+            System.out.println("Token logout: " + sharedPreferences.getString("session_token", "No value"));
+            jsonBody.put("session_token", sharedPreferences.getString("session_token", "No value"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.POST, url, jsonBody, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            System.out.println("MENSAJE: " + response);
+                            String result = response.getString("result");
+
+                            // Utiliza los valores extraídos según sea necesario
+                            if (result.equals("ok")) {
+                                System.out.println("S'ha tancat la sessió");
+                                Toast.makeText(GestorActivity.this,"Sessió tancada",Toast.LENGTH_SHORT).show();
+                                //Borrar SharedPreferences
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.clear();
+                                editor.apply(); // O también puedes usar editor.commit();
+
+                                Intent intent = new Intent(GestorActivity.this, welcome_page.class);
+                                startActivity(intent);
+
+                            } else {
+                                System.out.println("Token de sesió incorrecte");
+                                //Fer Pop-Up o algo per notificar l'usuari
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Error al realizar la solicitud
+                        error.printStackTrace();
+                    }
+                });
+
+        queue.add(jsonObjectRequest);
+        return exists;
     }
 
 }
