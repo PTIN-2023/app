635a6146e84f2d67324e40fafb8afb161dc86126
alexrocamora <72758624+alexrocamora@users.noreply.github.com>
2023-06-28 13:16:23 +0200

    mensaje "cerrar sesión" + logout
    Cuando se da el botón de ir hacia atrás en paciente, aparece un mensaje preguntando si está seguro de cerrar la sesión. En caso de aceptar, el user hacer un logout y cierra sesión.

--- app/src/main/java/com/example/appptin/MainActivity.java
+++ app/src/main/java/com/example/appptin/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.appptin;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
@@ -16,6 +17,7 @@ import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
@@ -142,46 +144,6 @@ public class MainActivity extends AppCompatActivity  implements ConfigPacienteFr
         fragmentTransaction.commit();
     }
 
-    //@Override
-    //Funció de barra de cerca
-    /*public boolean onCreateOptionsMenu(Menu menu) {
-
-        //getMenuInflater().inflate(R.menu.search_bar,menu);
-
-        MenuItem menuItem = menu.findItem(R.id.action_search);
-        SearchView searchView = (SearchView) menuItem.getActionView();
-        searchView.setQueryHint("Prem aquí per a cercar");
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                System.out.println("Test");
-                getMedicaments(query);
-                MedicamentsFragment MedicamentsFragment = new MedicamentsFragment();
-                // Reemplaza el fragmento actual con el MedicamentsFragment
-                replaceFragments(MedicamentsFragment);
-                // Tanquem el teclat virtual
-                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(searchView.getWindowToken(), 0);
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-
-                arrayAdapter.getFilter().filter(newText);
-                searchView.setVisibility(listView.VISIBLE);
-                return false;
-            }
-        });
-        return super.onCreateOptionsMenu(menu);
-    }*/
-
-    private void getMedicaments(String text){
-
-
-    }
-
     //Función para activar modo oscuro
     public void setDayNight(){
         SharedPreferences sp = getSharedPreferences("SP", this.MODE_PRIVATE);
@@ -302,5 +264,76 @@ public class MainActivity extends AppCompatActivity  implements ConfigPacienteFr
         queue.add(jsonObjectRequest);
     }
 
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Cerrar Sesión")
+                .setMessage("Seguro que quieres salir de tu sesión?")
+                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        logout();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
+    }
+
+    private boolean logout() {
+        boolean exists = false;
+        RequestQueue queue = Volley.newRequestQueue(this);
+        Resources r = getResources();
+        String apiUrl = r.getString(R.string.api_base_url);
+        String url = apiUrl + "/api/logout"; // Reemplaza con la dirección de tu API
+        System.out.println(url);
+        JSONObject jsonBody = new JSONObject();
+        SharedPreferences sharedPreferences = getSharedPreferences("UserPref", Context.MODE_PRIVATE);
+        try {
+            System.out.println("Token logout: " + sharedPreferences.getString("session_token", "No value"));
+            jsonBody.put("session_token", sharedPreferences.getString("session_token", "No value"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.POST, url, jsonBody, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            System.out.println("MENSAJE: " + response);
+                            String result = response.getString("result");
+
+                            // Utiliza los valores extraídos según sea necesario
+                            if (result.equals("ok")) {
+                                System.out.println("S'ha tancat la sessió");
+                                Toast.makeText(MainActivity.this,"Sessió tancada",Toast.LENGTH_SHORT).show();
+                                //Borrar SharedPreferences
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.clear();
+                                editor.apply(); // O también puedes usar editor.commit();
+
+                                Intent intent = new Intent(MainActivity.this, welcome_page.class);
+                                startActivity(intent);
+
+                            } else {
+                                System.out.println("Token de sesió incorrecte");
+                                //Fer Pop-Up o algo per notificar l'usuari
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Error al realizar la solicitud
+                        error.printStackTrace();
+                    }
+                });
+
+        queue.add(jsonObjectRequest);
+        return exists;
+    }
 
 }
